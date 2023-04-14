0918ff95b2c0efd2e8392f97673f9765f2d11ef4
wohool <1529063213@qq.com>
2023-04-14 21:54:49 +0800

    deploy

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -2,6 +2,10 @@ import type { DefaultTheme } from 'vitepress'
 
 export const nav: DefaultTheme.Config['nav'] = [
   {
+    text: '数据可视化平台',
+    link: 'https://sanmao147.github.io/vcharts/',
+  },
+  {
     text: '3d场景',
     link: 'https://sanmao147.github.io/three-react/',
   },
